49239ca47b19cc968015d984a7d7b96a22b929bf
João Pedro Aragão <jpedroarag@hotmail.com>
2019-08-19 11:29:21 -0300

    folder hierarchy change

---
+++ GarageFinderFramework/GarageFinderFramework.h
@@ -0,0 +1,19 @@
+//
+//  GarageFinderFramework.h
+//  GarageFinderFramework
+//
+//  Created by João Pedro Aragão on 19/08/19.
+//  Copyright © 2019 João Pedro Aragão. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GarageFinderFramework.
+FOUNDATION_EXPORT double GarageFinderFrameworkVersionNumber;
+
+//! Project version string for GarageFinderFramework.
+FOUNDATION_EXPORT const unsigned char GarageFinderFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GarageFinderFramework/PublicHeader.h>
+
+
