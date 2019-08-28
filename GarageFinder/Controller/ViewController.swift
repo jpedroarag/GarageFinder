3bfe762194a2c28d5c2fb3b08ae360a6f2e0a0c8
João Pedro Aragão <jpedroarag@hotmail.com>
2019-08-28 09:15:24 -0300

    Added file to fix merge

---
+++ GarageFinder/Controller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  GarageFinder
+//
+//  Created by João Pedro Aragão on 28/08/19.
+//  Copyright © 2019 João Pedro Aragão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
