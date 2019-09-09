8d6b1e8e77efc7608393bd9649c837336fc99087
João Pedro Aragão <jpedroarag@hotmail.com>
2019-09-09 10:35:45 -0300

    Make the close button round and added dismiss animation

--- GarageFinder/Controller/GarageDetailsViewController.swift
+++ GarageFinder/Controller/GarageDetailsViewController.swift
@@ -14,24 +14,19 @@ class GarageDetailsViewController: UIViewController {
        let button = UIButton()
         button.setTitle("X", for: .normal)
         button.backgroundColor = .gray
+        button.layer.cornerRadius = 15
         return button
     }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .blue
-        print("garage detail loaded")
-        
         view.addSubview(closeButton)
-        
-        closeButton.anchor
-        .top(view.topAnchor, padding: 16)
-        .right(view.rightAnchor, padding: 16)
-        .width(constant: 25)
-        .height(constant: 25)
-        
-        closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
-        
         view.frame = CGRect(x: 0, y: view.frame.height, width: view.frame.width, height: view.frame.height)
+        closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
+        setConstraints()
+        
         DispatchQueue.main.async {
             UIView.animate(withDuration: 0.3, animations: {
                 self.view.frame = CGRect(x: 0, y: 0,
@@ -40,11 +35,26 @@ class GarageDetailsViewController: UIViewController {
             })
         }
     }
+    
+    private func setConstraints() {
+        closeButton.anchor
+        .top(view.topAnchor, padding: 16)
+        .right(view.rightAnchor, padding: 16)
+        .width(constant: 30)
+        .height(constant: 30)
+//
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         
     }
+    
     @objc func closeButtonTapped() {
-        removeFromParent()
-        view.removeFromSuperview()
+        UIView.animate(withDuration: 0.3, animations: {
+            self.view.frame.origin.y = self.view.frame.height
+        }, completion: { _ in
+            self.removeFromParent()
+            self.view.removeFromSuperview()
+        })
     }
 }
